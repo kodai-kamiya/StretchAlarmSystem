62a20ed2cf001753b822e2e18d0b303c12f5be36
kangminFe <kangminfe@kangminFeui-MacBookAir.local>
2021-11-12 16:06:33 +0900

    add timer

--- pose_estimation.py
+++ pose_estimation.py
@@ -44,7 +44,7 @@ detector.hybridize()
 pose_net.hybridize()
 
 anglelist_rightarm = []
-anglelist_lefttarm = []
+anglelist_leftarm = []
 anglelist_rightleg = []
 anglelist_leftleg = []
 anglelist_rightelbow = []
@@ -53,10 +53,13 @@ anglelist_leftelbow = []
 
 # filepath = "motion2.MOV"
 
-cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture(filepath)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("motion.mov")
 time.sleep(1)
 
+# print(cap.get(cv2.CAP_PROP_FPS))
+start = time.time()
+result = 0 
 while(True):
 
     ret, frame = cap.read()
@@ -91,7 +94,10 @@ while(True):
                                                bounding_boxes,
                                                scores,
                                                box_thresh=0.5,
-                                               keypoint_thresh=0.2)
+                                               # keypoint_thresh=0.2)
+                                               keypoint_thresh=0)
+
+    pose_img = cv2.cvtColor(pose_img, cv2.COLOR_BGR2RGB)
 
     cv2.imshow('pose_img', pose_img)
 
@@ -99,66 +105,113 @@ while(True):
 
     ba = (pred_coords[0][7] - pred_coords[0][5]).asnumpy()
     bc = (pred_coords[0][11] - pred_coords[0][5]).asnumpy()
-    angle = angle_betweeen_two_vectors(ba, bc)
-    anglelist_rightarm.append(angle)
+    angle_rightarm = angle_betweeen_two_vectors(ba, bc)
+    anglelist_rightarm.append(angle_rightarm)
 
     de = (pred_coords[0][8] - pred_coords[0][6]).asnumpy()
     df = (pred_coords[0][12] - pred_coords[0][6]).asnumpy()
-    angle = angle_betweeen_two_vectors(de, df)
-    anglelist_lefttarm.append(angle)
+    angle_lefttarm = angle_betweeen_two_vectors(de, df)
+    anglelist_leftarm.append(angle_lefttarm)
 
     gh = (pred_coords[0][5] - pred_coords[0][11]).asnumpy()
     gi = (pred_coords[0][13] - pred_coords[0][11]).asnumpy()
-    angle = angle_betweeen_two_vectors(gh, gi)
-    anglelist_rightleg.append(angle)
+    angle_rightleg = angle_betweeen_two_vectors(gh, gi)
+    anglelist_rightleg.append(angle_rightleg)
 
     jk = (pred_coords[0][6] - pred_coords[0][12]).asnumpy()
     jl = (pred_coords[0][14] - pred_coords[0][12]).asnumpy()
-    angle = angle_betweeen_two_vectors(jk, jl)
-    anglelist_leftleg.append(angle)
+    angle_leftleg = angle_betweeen_two_vectors(jk, jl)
+    anglelist_leftleg.append(angle_leftleg)
 
     mn = (pred_coords[0][9] - pred_coords[0][7]).asnumpy()
     mo = (pred_coords[0][5] - pred_coords[0][7]).asnumpy()
-    angle = angle_betweeen_two_vectors(mn, mo)
-    anglelist_rightelbow.append(angle)
+    angle_rightelbow = angle_betweeen_two_vectors(mn, mo)
+    anglelist_rightelbow.append(angle_rightelbow)
 
     pq = (pred_coords[0][6] - pred_coords[0][8]).asnumpy()
     pr = (pred_coords[0][10] - pred_coords[0][8]).asnumpy()
-    angle = angle_betweeen_two_vectors(pq, pr)
-    anglelist_leftelbow.append(angle)
+    angle_leftelbow = angle_betweeen_two_vectors(pq, pr)
+    anglelist_leftelbow.append(angle_leftelbow)
+        
+    
+    if 160 <= angle_rightarm <= 180\
+        and 160 <= angle_lefttarm <= 180\
+            and 130 <= angle_rightelbow <= 180\
+                  and 130 <= angle_leftelbow <= 180 : 
+                      # and 130 <= angle_rightleg <= 180\
+                      #     and 130 <= angle_leftleg <= 180\
+                      print("GOOD")
+                      count = 0
+    else :
+        print("BAD")
+        count+1
 
+    if count < 10:
+        result = time.time() - start
+        result = int(result)/10
+        
+        print(f"timer：{result}")
+        
+    else :
+        print(f"timer：{result}")
 
+        
+            
 cap.release()
 cv2.destroyAllWindows()
 
-fig = plt.figure("histogram")
+# fig = plt.figure("histogram")
 
-ax1 = fig.add_subplot(2, 3, 1)
-ax1.set_title("Right Arm")
-ax2 = fig.add_subplot(2, 3, 2)
-ax2.set_title("left Arm")
-ax3 = fig.add_subplot(2, 3, 3)
-ax3.set_title("Right Leg")
-ax4 = fig.add_subplot(2, 3, 4)
-ax4.set_title("Left Leg")
-ax5 = fig.add_subplot(2, 3, 5)
-ax5.set_title("Right Elbow")
-ax6 = fig.add_subplot(2, 3, 6)
-ax6.set_title("Left Elbow")
+# ax1 = fig.add_subplot(2, 3, 1)
+# ax1.set_title("Right Arm")
+# ax2 = fig.add_subplot(2, 3, 2)
+# ax2.set_title("left Arm")
+# ax3 = fig.add_subplot(2, 3, 3)
+# ax3.set_title("Right Leg")
+# ax4 = fig.add_subplot(2, 3, 4)
+# ax4.set_title("Left Leg")
+# ax5 = fig.add_subplot(2, 3, 5)
+# ax5.set_title("Right Elbow")
+# ax6 = fig.add_subplot(2, 3, 6)
+# ax6.set_title("Left Elbow")
 
-bins = np.linspace(0, 180, 36)
+bins = np.arange(0, 180+1, 5)
 
-ax1.hist(anglelist_rightarm, bins)
-ax2.hist(anglelist_lefttarm, bins)
-ax3.hist(anglelist_rightleg, bins)
-ax4.hist(anglelist_leftleg, bins)
-ax5.hist(anglelist_rightelbow, bins)
-ax6.hist(anglelist_leftelbow, bins)
+# ax1.hist(anglelist_rightarm, bins)
+# ax2.hist(anglelist_lefttarm, bins)
+# ax3.hist(anglelist_rightleg, bins)
+# ax4.hist(anglelist_leftleg, bins)
+# ax5.hist(anglelist_rightelbow, bins)
+# ax6.hist(anglelist_leftelbow, bins)
+
+plt.figure()
+plt.xticks(np.arange(0, 180+1, 10))
+plt.hist(anglelist_rightarm, bins)
+
+plt.figure()
+plt.xticks(np.arange(0, 180+1, 10))
+plt.hist(anglelist_leftarm, bins)
+
+plt.figure()
+plt.xticks(np.arange(0, 180+1, 10))
+plt.hist(anglelist_rightleg, bins)
+
+plt.figure()
+plt.xticks(np.arange(0, 180+1, 10))
+plt.hist(anglelist_leftleg, bins)
+
+plt.figure()
+plt.xticks(np.arange(0, 180+1, 10))
+plt.hist(anglelist_rightelbow, bins)
+
+plt.figure()
+plt.xticks(np.arange(0, 180+1, 10))
+plt.hist(anglelist_leftelbow, bins)
 
 plt.tight_layout()
 plt.show()
 
-angles = np.stack((anglelist_rightarm, anglelist_lefttarm,
+angles = np.stack((anglelist_rightarm, anglelist_leftarm,
                    anglelist_rightelbow, anglelist_leftelbow, anglelist_rightleg, anglelist_leftleg))
 
 np.savetxt('data.txt', angles.T, fmt='%.5e')
